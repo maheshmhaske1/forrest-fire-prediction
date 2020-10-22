5cad33481762449158dcb2f15d04a262f6d2272d
Mahesh ambadas mhaske <70438274+maheshmhaske1@users.noreply.github.com>
2020-10-22 10:39:35 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import
+import time
+import matplotlib.pyplot as plt
+from plotly import graph_objs as go
+from sklearn.linear_model import LinearRegression
+import warnings
+
+print(warnings)
+data_complete=pd.read_csv('Forest_fire.csv')
+data_head=data_complete.head()
+data_tail=data_complete.tail()
+a=st.sidebar.radio('navigation',['show database','show visualization','use ML module','make module powerfull'])
+
+
+y0 = np.array(data_complete['Fire Occurrence']).reshape(-1,1)
+x0= np.array(data_complete['Temperature']).reshape(-1,1)
+x=data_complete.drop(data_complete.columns[[0,4]],axis=1)
+y=data_complete.drop(data_complete.columns[[0,1,2,3]],axis=1)
+lr=LinearRegression()
+lr.fit(x,y)
+
+if a=='show database':
+
+    if st.checkbox("complete database"):
+        st.text("showing complete database")
+        data_complete
+
+    elif st.checkbox('show hwad of database'):
+        st.text("showing head of database")
+        data_head
+
+    elif st.checkbox('show tail of database'):
+        st.text("showing tail if database")
+        data_tail
+
+
+
+elif a=='show visualization':
+    if st.checkbox("1"):
+        st.image('a.png')
+
+    elif st.checkbox('2'):
+        st.image('bb.png')
+
+
+    elif st.checkbox('3'):
+        st.image('dd.jpg')
+
+
+
+elif a=='use ML module':
+    oxygen1=st.number_input('oxygen')
+    temp1=st.number_input('temrature')
+    humidity1=st.number_input('humdity')
+    model=LinearRegression()
+    if st.button('predict'):
+        model.fit(x,y)
+        op=(model.predict([[oxygen1,temp1,humidity1]]))
+        op
+        if op >10:
+            st.warning('forrest in danger')
+        else:
+            st.success('forrest in safe')
+    r=st.radio('view data',['input','outout'])
+    if r=='input':
+        x
+    elif r=='outout':
+        y
+
+    elif st.checkbox('view all inputs'):
+        x
+    elif st.checkbox('view output on data'):
+        y
+    else:
+        st.spinner('in process')
+
+elif a=='make module powerfull':
+    st.spinner('midmsadlkasmaslkmclaskmck')
