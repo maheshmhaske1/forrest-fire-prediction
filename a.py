0ced50cb2ca3d4df978b0675b0254d680ace8495
Mahesh ambadas mhaske <70438274+maheshmhaske1@users.noreply.github.com>
2020-10-26 10:16:40 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from plotly import graph_objs as go
 from sklearn.linear_model import LinearRegression
 import warnings
 
